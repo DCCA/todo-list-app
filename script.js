795f1c2c755947c3c7cd255195a1e45793e62c3a
Daniel Andrade <dcca12@gmail.com>
2019-10-25 14:21:04 -0700

    *add style *add css reset *add favicon *add fontawesome *add google fonts

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const input = $('input')
+
 // Line-through when item is clicked
 $('ul').on('click','li',function(){
     $(this).toggleClass('done');
@@ -13,7 +15,16 @@ $('ul').on('click','.delete',function(event){
 $("input[type='text']").keypress(function(event){
     if(event.which == 13) {
         let val = $(this).val();
-        $('ul').append('<li><span class="delete">X</span>'+val+'</li>');
+        $('ul').append('<li><span class="delete"><i class="fa fa-trash"></i></span><p>'+val+'</p></li>');
         $(this).val('');
     }    
+})
+
+// Fade in or out the input field
+$('button').on('click', function(){
+    if($(input).css('display') == 'none'){
+        $(input).fadeIn(800);
+    } else{
+        $(input).fadeOut(500);
+    }
 })
