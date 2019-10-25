a282bd9ff17a43eac5ddcca76105913a6d26b46f
Daniel Andrade <dcca12@gmail.com>
2019-10-25 12:35:28 -0700

    *add delete itens * add create new todo

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 console.log('Conected');
 
-// Click on item to mark done or not done
-$('li').click(function(){
+
+// Click on X do delete item
+$('ul').on('click','li',function(){
     $(this).toggleClass('done');
 })
 
-// Click on X do delete item
-$('.delete').click(function(event){
-    $(this).parent().fadeOut();
+$('ul').on('click','.delete',function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+})
+
+// Create new todo
+$("input[type='text']").keypress(function(event){
+    if(event.which == 13) {
+        let val = $(this).val();
+        $('ul').append('<li><span class="delete">X</span>'+val+'</li>');
+        $(this).val('');
+    }    
 })
